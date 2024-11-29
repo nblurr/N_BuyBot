4964ee3332db561bb0b0b54d0dd0415f2be39260
admin@nblurr.com <admin@nblurr.com>
2024-11-29 09:12:18 -0500

    Fix + updates

--- bot.mjs
+++ bot.mjs
@@ -16,6 +16,8 @@ const POOLS = [
   "0x90e7a93e0a6514cb0c84fc7acc1cb5c0793352d2"  // N/WETH
 ];
 
+const contract_ABI = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "spender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }, { "internalType": "address", "name": "", "type": "address" }], "name": "allowance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_spender", "type": "address" }, { "internalType": "uint256", "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [{ "internalType": "bool", "name": "success", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "epoch", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "lastDoublingBlock", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "lastMintingBlock", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "mint", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "name", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "nextDoublingBlock", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_to", "type": "address" }, { "internalType": "uint256", "name": "_value", "type": "uint256" }], "name": "transfer", "outputs": [{ "internalType": "bool", "name": "success", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_from", "type": "address" }, { "internalType": "address", "name": "_to", "type": "address" }, { "internalType": "uint256", "name": "_value", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "internalType": "bool", "name": "success", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }];
+
 const pool_ABI = [
   {
     anonymous: false,
@@ -65,7 +67,7 @@ async function readJsonFileToObj(filename) {
 
 async function testNWETH(){
   try {
-    const details = await readJsonFileToObj("details_NWETH.json");
+    const details = await readJsonFileToObj("details_N_WETH.json");
 
     details.price = (details.price * 2).toFixed(2);
 
@@ -75,10 +77,31 @@ async function testNWETH(){
   }
 }
 
-// Track swaps
+function shortenAddress(address, startLength = 9, endLength = 9) {
+  if (address.length <= startLength + endLength) {
+      return address; // Return the original if it's already short
+  }
+  const start = address.substring(0, startLength);
+  const end = address.substring(address.length - endLength);
+  return `${start}....${end}`;
+}
+
+async function getNTotalSupply(){
+  try {
+    const contract = new ethers.Contract(N_TOKEN, contract_ABI, provider);
+    const totalSupply = await contract.totalSupply();
+
+    return ethers.utils.formatUnits(totalSupply, 0); // Assuming the token has 18 decimals
+  } catch (error) {
+      return 'NA';
+  }
+}
+
+// Track swap
 async function trackSwaps() {
   for (const pool of POOLS) {
     const poolContract = new ethers.Contract(pool, pool_ABI, provider);
+    const totalSupply = await getNTotalSupply();
 
     // Listen for the Swap event
     poolContract.on(
@@ -104,10 +127,16 @@ async function trackSwaps() {
             txHash: txHash,
             from: tx.from,
             to: tx.to,
-            event: event
+            event: event,
+            totalSupply: totalSupply
           };
 
-          writeJsonToFile(details, 'details.json');
+          if(details.pool == "0x5121f6d8954fc6086649b826026739881a8f80c2"){
+            writeJsonToFile(details, 'details_N_RFD.json');
+          } else {
+            writeJsonToFile(details, 'details_N_WETH.json');
+          }
+          
 
           processDetails(details);
         } catch (error) {
@@ -115,6 +144,20 @@ async function trackSwaps() {
         }
       }
     );
+  }
+}
+
+function getMC(details){
+  return Number((details.price/details.t1)*details.totalSupply).toFixed(0);;
+}
+
+function getPricePerN(details){
+  if (details.pool == "0x5121f6d8954fc6086649b826026739881a8f80c2") {
+    // N_RFD
+    return Number(details.price/details.t1).toFixed(4);
+  } else {
+    // N_WETH ** TO ADJUST ** 
+    return Number(details.price/details.t1).toFixed(4);
   }
 }
 
@@ -134,22 +177,24 @@ async function processDetails(details){
     if(details.t0>0) {
       $msg =`💸💸 N/RFD 💸💸
 
-🤵‍♂️ From: <a href='https://etherscan.io/address/${details.from}'>details.from</a>
-🤵‍♂️ To: <a href='https://etherscan.io/address/${details.to}'>details.to</a>
+🤵‍♂️ From: <a href='https://etherscan.io/address/${details.from}'>${shortenAddress(details.from)}'</a>
+🤵‍♂️ To: <a href='https://etherscan.io/address/${details.to}'>${shortenAddress(details.to)}'</a>
 💱 Swapped N: ${details.t1}
 💱 Got RFD: ${details.t0}
-💵 Price (USD) - : ${details.price}
-🔍 <a href='https://etherscan.io/tx/${details.txHash}'>TX</a>
+💵 N price: ${getPricePerN(details)}$
+💵 <a href='https://etherscan.io/tx/${details.txHash}'>TX</a> Price (USD) - : ${details.price}
+💵 MC N/RFD : ${getMC(details)}$
       `;
     } else {
       $msg =`💸💸 N/RFD 💸💸
 
-🤵‍♂️ From: <a href='https://etherscan.io/address/${details.from}'>details.from</a>
-🤵‍♂️ To: <a href='https://etherscan.io/address/${details.to}'>details.to</a>
+🤵‍♂️ From: <a href='https://etherscan.io/address/${details.from}'>${shortenAddress(details.from)}'</a>
+🤵‍♂️ To: <a href='https://etherscan.io/address/${details.to}'>${shortenAddress(details.to)}'</a>
 💱 Swapped RFD: ${details.t0}
 💱 Received N: ${details.t1}
-💵 Price (USD) : ${details.price}
-🔍 <a href='https://etherscan.io/tx/${details.txHash}'>TX</a>
+💵 N price: ${getPricePerN(details)}$
+💵 <a href='https://etherscan.io/tx/${details.txHash}'>TX</a> Price (USD) : ${details.price}
+💵 MC N/RFD : ${getMC(details)}$
       `;
     }
 
@@ -158,12 +203,13 @@ async function processDetails(details){
     if(details.t0>0) {
       $msg =`💸💸 N/ETH 💸💸
 
-🤵‍♂️ From: <a href='https://etherscan.io/address/${details.from}'>${details.from}</a>
-🤵‍♂️ To: <a href='https://etherscan.io/address/${details.to}'>${details.to}</a>
+🤵‍♂️ From: <a href='https://etherscan.io/address/${details.from}'>${shortenAddress(details.from)}</a>
+🤵‍♂️ To: <a href='https://etherscan.io/address/${details.to}'>${shortenAddress(details.to)}</a>
 💱 Swapped N: ${details.t1}
 💱 Received ETH: ${details.t0}
-💵 Price (USD) : ${details.price}
-🔍 <a href='https://etherscan.io/tx/${details.txHash}'>TX</a>
+💵 N price: ${getPricePerN(details)}$
+💵 <a href='https://etherscan.io/tx/${details.txHash}'>TX</a> Price (USD) : ${details.price}$
+💵 MC N/WETH : ${getMC(details)}$
       `;
     } else {
       $msg =`💸💸 N/ETH 💸💸
@@ -172,8 +218,9 @@ async function processDetails(details){
 🤵‍♂️ To: <a href='https://etherscan.io/address/${details.to}'>${details.to}<a>
 💱 Swapped ETH: ${details.t0}
 💱 Received N: ${details.t1}
-💵 Price (USD) : ${details.price}
-🔍 <a href='https://etherscan.io/tx/${details.txHash}'>TX</a>
+💵 N price: ${getPricePerN(details)}$
+💵 <a href='https://etherscan.io/tx/${details.txHash}'>TX</a> Price (USD) : ${details.price}$
+💵 MC N/WETH: ${getMC(details)}$
       `;
     }
   }
